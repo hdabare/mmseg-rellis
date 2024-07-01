e14d66c8cb4ef2a826a1fc3d2ba5b291acdcdcf9
Hewage Dabare <h.dabare@ecu.edu.au>
2024-07-01 11:54:22 +0800

    Modify img_scale to scale in rellis.py train_pipeline and test_pipeline transforms

--- configs/_base_/datasets/rellis.py
+++ configs/_base_/datasets/rellis.py
@@ -8,7 +8,7 @@ crop_size = (375, 600)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=img_size, ratio_range=(0.5, 2.0)),
+    dict(type='Resize', scale=img_size, ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -21,7 +21,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=crop_size,
+        scales=[crop_size],
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
