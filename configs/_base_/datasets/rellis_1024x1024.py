36a95689073a7d0bf175eaf8b521366c1b7eb645
Hewage Dabare <h.dabare@ecu.edu.au>
2024-07-02 12:22:56 +0800

    Modify rellis 1024x1024 config to remove tta_pipeline Add ddrnet rellis 20k config

--- configs/_base_/datasets/rellis_1024x1024.py
+++ configs/_base_/datasets/rellis_1024x1024.py
@@ -1,19 +1,18 @@
 # dataset settings
 dataset_type = 'RELLISDataset'
 data_root = '../GANav-offroad/data/rellis/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-img_size = (1920, 1200)
 crop_size = (1024, 1024)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', scale=(2048, 1024), scale_factor=(0.5, 2.0)),
-    dict(type='RandomCrop', crop_size=crop_size),
+    dict(
+        type='RandomResize',
+        scale=(2048, 1024),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size=crop_size, pad_val=0),
     dict(type='PackSegInputs')
 ]
 test_pipeline = [
@@ -23,22 +22,6 @@ test_pipeline = [
     # does not need to do resize data transform
     dict(type='LoadAnnotations'),
     dict(type='PackSegInputs')
-]
-img_ratios = [0.5, 0.75, 1.0, 1.25, 1.5, 1.75]
-tta_pipeline = [
-    dict(type='LoadImageFromFile', backend_args=None),
-    dict(
-        type='TestTimeAug',
-        transforms=[
-            [
-                dict(type='Resize', scale_factor=r, keep_ratio=True)
-                for r in img_ratios
-            ],
-            [
-                dict(type='RandomFlip', prob=0., direction='horizontal'),
-                dict(type='RandomFlip', prob=1., direction='horizontal')
-            ], [dict(type='LoadAnnotations')], [dict(type='PackSegInputs')]
-        ])
 ]
 
 train_dataloader = dict(
